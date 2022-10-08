00ad22ddd4d49ce9c6b879209ba0a1e6499b93ff
Varun Narayanswamy <varun@Varuns-MacBook-Pro-2.local>
2022-10-08 16:46:08 -0600

    Attempting to understand subtree

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/varunknarayanswamy.github.io/',
+  publicPath: '/',
 })
